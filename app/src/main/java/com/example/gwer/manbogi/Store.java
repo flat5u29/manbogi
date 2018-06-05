7d9e6738a09032d3393e29ed6745d700223ff37c
PJE <pje010101@naver.com>
2018-06-05 13:24:38 +0900

    환전소리 추가(동전소리) 다른 버튼소리 추가하려면 onCreate 안에 final int 소리이름 = sp.load(this, R.raw.파일명, 1); 버튼 onClick 안에 sp.play(소리이름, 1, 1, 0, 0, 1f);

--- app/src/main/java/com/example/gwer/manbogi/Store.java
+++ app/src/main/java/com/example/gwer/manbogi/Store.java
@@ -7,16 +7,35 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Store extends AppCompatActivity {
 
-
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        btn = (Button)findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int cost = 30;
+                MainActivity mainActivity = new MainActivity();
+                if(mainActivity.coin >= cost) {
+                    mainActivity.coin = mainActivity.coin - cost;
+                    Toast.makeText(getApplicationContext(),"구매되었습니다. 남은코인 : "+mainActivity.coin, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "코인이 부족합니다."+mainActivity.coin, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
 }
